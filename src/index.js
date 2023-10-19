c734fa72a5df80a4a56e9b75cb755919ccae7f41
chetan pal <cpal22387@gmail.com>
2023-10-19 20:33:41 +0530

    Added proxy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { rateLimit } = require('express-rate-limit')
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
@@ -13,7 +14,8 @@ const limiter = rateLimit({
 	limit: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes)
 
 })
-
+app.use('/flightsService', createProxyMiddleware({ target: ServerConfig.FLIGHT_SERVICE, changeOrigin: true }));   
+app.use('/bookingService', createProxyMiddleware({ target: ServerConfig.BOOKING_SERVICE, changeOrigin: true }));
 app.use(limiter)
 app.use('/api', apiRoutes);
 
